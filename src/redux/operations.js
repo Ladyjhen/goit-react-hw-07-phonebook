e8b8ff4e92b9bbb4a601a69c1eea31355f0dd1f0
Ladyjhen <145974043+Ladyjhen@users.noreply.github.com>
2024-07-22 00:26:41 +0800

    commit

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,29 +3,28 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://669d1b6115704bb0e30526bc.mockapi.io/api/v1/';
 
-// Se definește si se exporta o acțiune asincronă numită 'fetchContacts' folosind createAsyncThunk:
-export const fetchContacts = createAsyncThunk(
-  'contacts/fetchAll', // Numele acțiunii (în acest caz, 'contacts/fetchAll')
 
-  // Callback asincron invocat când acțiunea este declanșată (dispatch):
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll', 
+
+ 
   async (_, thunkAPI) => {
     try {
-      // Se realizează o cerere GET către server pentru a obține datele despre contacte:
+     
       const response = await axios.get('/contacts');
 
-      // Afișează datele în consolă:
+     
       console.log('Datele din API:', response.data);
 
-      // Returnează datele obținute de la server:
+      
       return response.data;
     } catch (error) {
-      // În caz de eroare, se respinge cu o valoare care include mesajul de eroare:
+      
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-// ==== Actiunea asincrona addContact: ====
 export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
@@ -38,7 +37,7 @@ export const addContact = createAsyncThunk(
   }
 );
 
-// ==== Actiunea asincrona deleteContact: ====
+
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, thunkAPI) => {
